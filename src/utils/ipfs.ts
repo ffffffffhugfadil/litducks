c7c16ee24d5d41972545f683d51e7512fd4616d0
ffffffffhugfadil <fadilajipasopati6@gmail.com>
2026-06-10 23:03:40 +0700

    Update ipfs.ts

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -3,18 +3,23 @@ export function ipfsToUrl(ipfsUrl: string | undefined): string {
   if (!ipfsUrl) return ''
   
 
-  const GATEWAY = 'https://gateway.pinata.cloud/ipfs/'
-  
-  if (ipfsUrl.startsWith('ipfs://')) {
-    return ipfsUrl.replace('ipfs://', GATEWAY)
-  }
-  
-  if (ipfsUrl.startsWith('/ipfs/')) {
-    return ipfsUrl.replace('/ipfs/', GATEWAY)
-  }
-  
-  if (ipfsUrl.startsWith('http')) {
+  if (ipfsUrl.startsWith('http://') || ipfsUrl.startsWith('https://')) {
     return ipfsUrl
+  }
+  
+
+  if (ipfsUrl.startsWith('ipfs://')) {
+    return ipfsUrl.replace('ipfs://', 'https://gateway.pinata.cloud/ipfs/')
+  }
+  
+
+  if (ipfsUrl.startsWith('/ipfs/')) {
+    return ipfsUrl.replace('/ipfs/', 'https://gateway.pinata.cloud/ipfs/')
+  }
+  
+
+  if (ipfsUrl.startsWith('Qm') && ipfsUrl.length === 46) {
+    return `https://gateway.pinata.cloud/ipfs/${ipfsUrl}`
   }
   
   return ipfsUrl
